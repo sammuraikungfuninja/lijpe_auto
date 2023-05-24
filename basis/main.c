59e423a62ba0ec64a91f5d6d5fe545b5ae27ace4
Daan Boekestijn <daanboek03@gmail.com>
2023-05-24 15:35:42 +0200

    opzet voor code

--- basis/main.c
+++ basis/main.c
@@ -2,14 +2,82 @@
 haiiiiiiii
  */
 #include <avr/io.h>
+#include "h_bridge.h"
+#include "h_bridge.c"
 
+void vroem()
+{
+    h_bridge_set_percentage(50);
+}
+
+void plantsensoraan()
+{
+    //sensoren aan
+}
+
+void rem()
+{
+    h_bridge_set_percentage(0);
+}
+
+void alarm()
+{
+    //1 pinnetje aan en uit schakelen voor 2x
+}
+
+void stuurlinks()
+{
+    //servo
+}
+
+void stuurrechts()
+{
+    //servo
+}
+
+int state;
 int main(void)
 {
-
-    // Insert code
+    init_h_bridge();
 
     while(1)
-    ;
+    {
+        //sensoren checken
+
+        switch(state)
+        {
+        case(0):
+            rem();
+            break;
+
+        case(1):
+            vroem();
+            plantsensoraan();
+            //zij sensoren en sturen bijhouden
+            break;
+
+        case(2):
+            rem();
+            alarm();
+            //plant detect
+            //alarm
+            break;
+
+        case(3):
+            //bocht
+            //plant sensoren uit
+            break;
+
+        case(4):
+            stuurlinks();
+            break;
+
+        case(5):
+            stuurrechts();
+            break;
+        }
+    }
+
 
     return 0;
 }
