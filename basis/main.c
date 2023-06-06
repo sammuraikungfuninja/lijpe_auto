f4c5cec75e8c1fac75c2d88ae099df25c40e7851
Naman Kayastha <22086684@student.hhs.nl>
2023-06-06 12:15:05 +0200

    sensor functie

--- basis/main.c
+++ basis/main.c
@@ -15,7 +15,7 @@ daan is brokko
 #include "servo.c"
 #include <avr/interrupt.h>
 #include <util/delay.h>
-int state;
+int state = 0;
 int random1 = 0;        //ints bij buzzer
 int random2 = 0;
 void init()
@@ -38,6 +38,37 @@ void init()
     PORTB &= ~(1<<PB1);
 }
 
+void sensoren()
+{
+        if ((PINF & (1<<PF0))==0)       //beide zijkanten checken, rechtdoor
+        {
+        if ((PINF & (1<<PF1))==0)
+        {
+            state = 1;
+        }
+        }
+        if (PINF & (1<<PF0))                   //links checken, rechts corigeren
+        {
+            state = 5;
+        }
+        if (PINF & (1<<PF1))                    //rechts checken, links corigeren
+        {
+            state = 4;
+        }
+            if (PINF & (1<<PF5)==0)                 //balk checken, grote bocht
+        {
+        state = 3;
+        }
+
+
+}
+void veiligheidsensor()
+{
+    if (PINF & (1<<PF4)==0)                        //voren iets, staan stil
+        {
+            state = 0;
+        }
+}
 void vroem()
 {
     h_bridge_set_percentage(50);
@@ -45,7 +76,7 @@ void vroem()
 
 void plantsensoraan()
 {
-if(random1 == 0)
+if(random1 <= 0)
 {
 if ((PINF & (1<<PF2))==0)                    //bomen rechts, staan stil
 {
@@ -57,7 +88,7 @@ else
 {
     random1--;
 }
-if (random2 == 0)
+if (random2 <= 0)
 {
 if ((PINF & (1<<PF3))==0)                    //bomen links, staan stil
 {
@@ -123,63 +154,51 @@ int main(void)
 
     {
         //sensoren checken
-        if ((PINF & (1<<PF0))==0)       //beide zijkanten checken, rechtdoor
-        {
-        if ((PINF & (1<<PF1))==0)
-        {
-            state = 1;
-        }
-        }
-        if (PINF & (1<<PF0))                   //links checken, rechts corigeren
-        {
-            state = 5;
-        }
-        if (PINF & (1<<PF1))                    //rechts checken, links corigeren
-        {
-            state = 4;
-        }
-            if (PINF & (1<<PF5)==0)                 //balk checken, grote bocht
-        {
-        state = 3;
-        }
 
-        if (PINF & (1<<PF4)==0)                        //voren iets, staan stil
-        {
-            state = 0;
-        }
         state = 1;
         switch(state)
 
         {
         case(0):
             rem();
+            sensoren();
+            veiligheidsensor();
             break;
 
         case(1):
             stuurvooruit();
             vroem();
+            sensoren();
             plantsensoraan();
-            //zij sensoren en sturen bijhouden
+            veiligheidsensor();//zij sensoren en sturen bijhouden
             break;
 
         case(2):
             rem();
             alarm();
             vroem();
+            sensoren();
+            veiligheidsensor();
             break;
 
         case(3):
             grotebocht();//bocht
+            sensoren();
+            veiligheidsensor();
             break;
 
         case(4):
             stuurlinks();
+            sensoren();
             plantsensoraan();
+            veiligheidsensor();
             break;
 
         case(5):
             stuurrechts();
+            sensoren();
             plantsensoraan();
+            veiligheidsensor();
             break;
         }
     }
