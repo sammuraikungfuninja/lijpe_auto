63a4b6fbd6d538bdaf0a452a37057790047d2fa8
Ilmar Nan <ilmar150b@gmail.com>
2023-06-19 14:37:12 +0200

    vgm werkt alles :]

--- basis/main.c
+++ basis/main.c
@@ -15,22 +15,25 @@ daan is brokko
 #include "servo.c"
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#define RESET 34534ul
+#define RESET 34000ul
+#define DEBOUNCE _delay_ms(110)
 int state = 0;
 volatile int random1 = 1;        //ints bij buzzer
 volatile int random2 = 1;
+volatile int random3 = 1;
+volatile int tellerbocht = 2;
 void init()
 {
-    DDRF &= ~(1<<PF6); //A6, ir links voor
-    PORTF |= (1<<PF6);
-    DDRF &= ~(1<<PF1); //A1, ir rechts voor
+    DDRD &= ~(1<<PD3); //D18, ir rechts voor
+    PORTD |= (1<<PD3);
+    DDRF &= ~(1<<PF1); //A1, ir links voor
     PORTF |= (1<<PF1);
     DDRF &= ~(1<<PF2); //A2, ir bomen rechts
     PORTF |= (1<<PF2);
     DDRF &= ~(1<<PF3);//A3, ir bomen links
     PORTF |= (1<<PF3);
-    DDRF &= ~(1<<PF4);//A4, voren detecteren
-    PORTF |= (1<<PF4);
+    DDRD &= ~(1<<PD2);// D19, voren detecteren
+    PORTD |= (1<<PD2);
     DDRF &= ~(1<<PF5);//A5, eind van balk detectie
     PORTF |= (1<<PF5);
     DDRB |= (1<<PB0);  //53, links leds
@@ -47,36 +50,71 @@ ISR(TIMER3_OVF_vect)
     TCCR3B = 0;
 }
 
+ISR(TIMER4_OVF_vect)
+{
+    tellerbocht++;
+
+}
+
 void sensoren()
 {
-        if ((PINF & (1<<PF0))==0)       //beide zijkanten checken, rechtdoor
+        if ((PIND & (1<<PD3)))       //beide zijkanten checken, rechtdoor
         {
-        if ((PINF & (1<<PF1))==0)
+        if ((PINF & (1<<PF1)))
         {
+            DEBOUNCE;
             state = 1;
         }
         }
-        if (PINF & (1<<PF0))                   //links checken, rechts corigeren
+
+        if ((PINF & (1<<PF1))==0)                    //rechts checken, links corigeren
         {
+            DEBOUNCE;
             state = 5;
         }
-        if (PINF & (1<<PF1))                    //rechts checken, links corigeren
+
+        if ((PIND & (1<<PD3))==0)                   //links checken, rechts corigeren
         {
+            DEBOUNCE;
             state = 4;
         }
-            if (PINF & (1<<PF5)==0)                 //balk checken, grote bocht
+        if ((PIND & (1<<PD3))==0)       //beide zijkanten checken, stoppen
         {
+        if ((PINF & (1<<PF1))==0)
+        {
+            DEBOUNCE;
+            state = 0;
+        }
+        }
+       if(tellerbocht >= 2)                      //te vroeg achtersensor aan 2 moet groter
+        {
+            if (PINF & (1<<PF5))                 //balk checken, grote bocht
+        {
+        DEBOUNCE;
         state = 3;
+        }
         }
 
 
 }
 void veiligheidsensor()
 {
-    if (PINF & (1<<PF4)==0)                        //voren iets, staan stil
-        {
-            state = 0;
-        }
+   if ((PIND & (1<<PD2))==0)                        //voren iets, staan stil
+    {
+        state = 0;
+    }
+}
+
+void bochtveiligheid()
+{
+    if ((PIND & (1<<PD2))==0)                        //voren iets, staan stil
+    {
+        state = 6;
+    }
+    else
+    {
+        state = 3;
+    }
 }
 void vroem()
 {
@@ -85,23 +123,23 @@ void vroem()
 
 void plantsensoraan()
 {
-if(random1 == 1)
-{
-if ((PINF & (1<<PF2))==0)                    //bomen rechts, staan stil
-{
-state = 2;
-random1 = 0;
-}
-}
+    if(random1 == 1)
+    {
+        if ((PINF & (1<<PF2))==0)                    //bomen rechts, staan stil
+        {
+            state = 2;
+            random1 = 0;
+        }
+    }
 
-if (random2 == 1)
-{
-if ((PINF & (1<<PF3))==0)                    //bomen links, staan stil
-{
-state = 2;
-random2 = 0;
-}
-}
+    if (random2 == 1)
+    {
+        if ((PINF & (1<<PF3))==0)                    //bomen links, staan stil
+        {
+            state = 2;
+            random2 = 0;
+        }
+    }
 }
 
 
@@ -145,7 +183,30 @@ void stuurvooruit()
 
 void grotebocht()
 {
-
+    if (random3 == 1)
+    {
+    servo1_set_percentage(-20);
+    DEBOUNCE;
+    if ((PIND & (1<<PD3))==0)                    //links checken, rechts corigeren
+    {
+        state = 1;
+       tellerbocht = 0;
+        TCNT4 = 0;
+        random3 = 2;
+    }
+    }
+    if (random3 == 2)
+    {
+    servo1_set_percentage(20);
+    DEBOUNCE;
+    if ((PINF & (1<<PF1))==0)                    //links checken, rechts corigeren
+    {
+        state = 1;
+       tellerbocht = 0;
+        TCNT4 = 0;
+        random3 = 1;
+    }
+    }
 }
 
 int main(void)
@@ -155,6 +216,10 @@ int main(void)
     TIMSK3 |= (1<<TOIE3);
     TCNT3 = RESET;
 
+    TCCR4A = 0;
+    TCCR4B |= ((1<<CS42)|(0<<CS41)|(1<<CS40));
+    TIMSK4 |= (1<<TOIE4);
+
     sei();
     init_h_bridge();    //d12,13
     init_servo();       //d11
@@ -162,14 +227,13 @@ int main(void)
     while(1)
 
     {
-        //sensoren checken
-
 
         switch(state)
 
         {
         case(0):
             rem();
+            stuurvooruit();
             sensoren();
             veiligheidsensor();
             break;
@@ -191,13 +255,14 @@ int main(void)
             break;
 
         case(3):
+            vroem();
+            bochtveiligheid();
             grotebocht();//bocht
-            sensoren();
-            veiligheidsensor();
             break;
 
         case(4):
             stuurlinks();
+            vroem();
             sensoren();
             plantsensoraan();
             veiligheidsensor();
@@ -205,10 +270,15 @@ int main(void)
 
         case(5):
             stuurrechts();
+            vroem();
             sensoren();
             plantsensoraan();
             veiligheidsensor();
             break;
+
+        case(6):
+            rem();
+            bochtveiligheid();
         }
     }
 
